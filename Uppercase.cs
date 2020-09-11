c306bfd9c5e764c09105fe2a1b6837ea8faa3b42
ThashinaSultana <70582506+ThashinaSultana@users.noreply.github.com>
2020-09-11 10:15:05 +0530

    Add files via upload

---
+++ Uppercase.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Console.Write("Enter The String Need to be Capitalized: ");
+            string str = Console.ReadLine();
+            string titleCase = CapitalizeFirstLetter(str);
+            
+            Console.WriteLine("Capitalized String: " + titleCase);
+
+
+            Console.Read();
+        }
+
+        static string CapitalizeFirstLetter(string value)
+        {
+            
+            value = value.ToLower();
+            char[] array = value.ToCharArray();
+            
+            if (array.Length >= 1)
+            {
+                if (char.IsLower(array[0]))
+                {
+                    array[0] = char.ToUpper(array[0]);
+                }
+            }
+            
+           for (int i = 1; i < array.Length; i++)
+            {
+                if (array[i - 1] == ' ')
+                {
+                    if (char.IsLower(array[i]))
+                    {
+                        array[i] = char.ToUpper(array[i]);
+                    }
+                }
+            }
+            return new string(array);
+        }
+    }
+
+}
+
+
